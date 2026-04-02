24daa186c47d88e8a7211fed849c723c20d53b9e
vinonsan <vinonsan.99@gmail.com>
2026-04-02 13:04:30 +0530

    feat(hero-section): design a base template for hero section for home page

--- app/(public)/_utils/PageChildren.tsx
+++ app/(public)/_utils/PageChildren.tsx
@@ -1,22 +1,11 @@
 import { Fragment } from "react/jsx-runtime";
-import HomeCategories from "./components/HomeCategories";
-import HomeHeader from "./components/HomeHeader";
-import HomeHero from "./components/HomeHero";
-import HomeOccasions from "./components/HomeOccasions";
-import HomeWhyChoose from "./components/HomeWhyChoose";
+import Hero from "./components/Hero";
+
 
 export default function PageChildren() {
   return (
     <Fragment>
-        <HomeHeader />
-
-        <div className="grid items-center gap-12 lg:grid-cols-[1.15fr_0.85fr]">
-          <HomeHero />
-          <HomeCategories />
-        </div>
-
-        <HomeWhyChoose />
-        <HomeOccasions />
+      <Hero />
     </Fragment>
   );
 }
