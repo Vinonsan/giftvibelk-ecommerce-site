6f1fa68be4c91cf5d3fe561c1d7c90a9c73b9d02
vinonsan <vinonsan.99@gmail.com>
2026-04-14 16:12:15 +0530

    feat(about-page): implement about page layout design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
 import Footer from "./components/layouts/Footer";
-import { Geist } from "next/font/google";
 import Header from "./components/layouts/Header";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: {
@@ -53,7 +47,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en-LK" className={`${geistSans.variable} h-full w-full antialiased`}>
+    <html lang="en-LK" className="h-full w-full antialiased">
      
       <body suppressHydrationWarning className="flex flex-col">
          <Header />
